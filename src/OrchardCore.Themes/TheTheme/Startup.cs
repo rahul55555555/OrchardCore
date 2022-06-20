c6eb476f3c11fd5f4ec803523d0aa92dd346ca19
rahul55555555 <106815878+rahul55555555@users.noreply.github.com>
2022-06-20 18:27:35 +0530

    Update Startup.cs

--- src/OrchardCore.Themes/TheTheme/Startup.cs
+++ src/OrchardCore.Themes/TheTheme/Startup.cs
@@ -9,6 +9,8 @@ namespace OrchardCore.Themes.TheTheme
         public override void ConfigureServices(IServiceCollection serviceCollection)
         {
             serviceCollection.AddScoped<IResourceManifestProvider, ResourceManifest>();
+            
+            serviceCollection.AddScoped<IResourceManifestProvider, ResourceManifest>();
         }
     }
 }
